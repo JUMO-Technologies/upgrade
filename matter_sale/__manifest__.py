e7c3c4c40fff34ab27fd49c0a801a7bd8e4a03a8
Vladimir Alvarez <vladimir881002@gmail.com>
2022-01-06 00:42:11 +0000

    [FIX]Purchase picking in order

--- matter_sale/__manifest__.py
+++ matter_sale/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'JUMO Technologies',
     'website': '',
     'license': '',
-    'depends': ['sale_quotation_builder', 'sale_stock', 'purchase_stock'],
+    'depends': ['sale_quotation_builder', 'sale_stock', 'purchase_stock', 'sale_purchase'],
     'data': [
         'views/crm_team.xml',
         'views/sale_order.xml',
--- matter_sale/models/sale_order.py
+++ matter_sale/models/sale_order.py
@@ -7,6 +7,14 @@ class SaleOrder(models.Model):
 
     team_warehouse_id = fields.Many2one(related="team_id.warehouse_id", string="Team Warehouse")
     warehouse_id = fields.Many2one(domain="[('id', '=', team_warehouse_id)]")
+    purchase_ids = fields.One2many('purchase.order', 'sale_order_id')
+    purchase_picking_counts = fields.Integer("Number of Purchase Pickings", compute='_compute_purchase_picking_count',
+                                             groups='purchase.group_purchase_user')
+
+    @api.depends('order_line.purchase_line_ids')
+    def _compute_purchase_picking_count(self):
+        for order in self:
+            order.purchase_picking_counts = len(order.purchase_ids.mapped('picking_ids'))
 
     @api.onchange('team_id')
     def _onchange_team_warehouse(self):
@@ -30,3 +38,27 @@ class SaleOrder(models.Model):
             if sale.state in ['draft', 'sent'] and sale.team_id and sale.warehouse_id and \
                     sale.warehouse_id.id != sale.team_id.warehouse_id.id:
                 raise ValidationError(_("The chosen warehouse is not available to this sales team."))
+
+    def action_view_purchase_picking(self):
+        self.ensure_one()
+        action = self.env.ref('stock.action_picking_tree_all')
+        result = action.read()[0]
+        # override the context to get rid of the default filtering on operation type
+        purchase_ids = self.purchase_ids
+        pick_ids = purchase_ids.mapped('picking_ids')
+
+        if purchase_ids:
+            result['context'] = {'default_partner_id': purchase_ids[0].partner_id.id, 'default_origin': purchase_ids[0].name,
+                                 'default_picking_type_id': purchase_ids[0].picking_type_id.id}
+        # choose the view_mode accordingly
+        if not pick_ids or len(pick_ids) > 1:
+            result['domain'] = "[('id','in',%s)]" % (pick_ids.ids)
+        elif len(pick_ids) == 1:
+            res = self.env.ref('stock.view_picking_form', False)
+            form_view = [(res and res.id or False, 'form')]
+            if 'views' in result:
+                result['views'] = form_view + [(state,view) for state,view in result['views'] if view != 'form']
+            else:
+                result['views'] = form_view
+            result['res_id'] = pick_ids.id
+        return result
