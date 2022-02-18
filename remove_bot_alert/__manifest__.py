759f345acd6512c3ea89818e50c5adca45949e97
Vladimir Alvarez <vladimir881002@gmail.com>
2022-02-18 04:07:22 +0000

    [FIX]remove_bot_alert

---
+++ remove_bot_alert/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Remove Bot Alert',
+    'version': '13.0.0.0.1',
+    'summary': 'Remove Bot Alert',
+    'description': 'Remove Bot Alert',
+    'category': 'Stock',
+    'author': 'Jumo Technologies',
+    'website': 'https://www.jumotech.com',
+    'license': 'LGPL-3',
+    'depends': ['stock'],
+    'installable': True,
+    'auto_install': False
+}
