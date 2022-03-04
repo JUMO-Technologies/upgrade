7d6323ec9b64b24c6276ff59be766e97f9990025
Jorge Luis López Ricardo <jorgeluislr93@gmail.com>
2022-03-04 03:51:40 +0000

    [ADD]load_order_report module

---
+++ load_order_report/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Loading Order Report',
+    'version': '13.0.0.0.1',
+    'summary': 'Loading Order Report',
+    'description': 'Loading Order Report',
+    'category': 'Category',
+    'author': 'Jumo Technologies',
+    'website': 'https://www.jumotech.com',
+    'license': 'LGPL-3',
+    'depends': ['purchase_stock'],
+    'data': [
+        "reports/stock_picking.xml",
+    ],
+    'installable': True,
+    'auto_install': False
+}
