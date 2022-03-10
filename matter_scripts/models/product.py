19039da348e24e0f3432586cf9c0ec1353c97b7b
Jorge Luis López Ricardo <jorgeluislr93@gmail.com>
2022-03-10 06:02:28 +0000

    [FIX]Script product update

--- matter_scripts/models/product.py
+++ matter_scripts/models/product.py
@@ -7,39 +7,25 @@ class ProductTemplate(models.Model):
     def script_rename_product(self):
         if not self.user_has_groups("base.group_system"):
             return
-        self._cr.execute("""SELECT prod.id FROM product_template prod
+        sql = """UPDATE ir_translation it SET src = prods.name, value = '' {} 
+        WHERE it.res_id = prods.id AND it.name = 'product.public.category,name';
+        """.format(self._get_from_clause())
+
+        sql2 = """UPDATE product_template pt SET name = prods.name {}
+        WHERE pt.id = prods.id;""".format(self._get_from_clause())
+        self._cr.execute(sql)
+        self._cr.execute(sql2)
+
+    def _get_from_clause(self):
+        return """FROM (SELECT prod.id, CONCAT(xc.collection_number, ' ', xc.name, ' ', xcc.x_name, ' ', 
+        xfc.x_name, ' ',  CASE WHEN prod.x_product_rect THEN 'R ' ELSE '' END, xac.x_name, ' ', 
+        prod.x_product_name_desc) AS name
+        FROM product_template prod
+        INNER JOIN product_public_category xc ON xc.id = prod.public_category_id
         INNER JOIN x_colores_coleccion xcc ON xcc.id = prod.x_producto_color
         INNER JOIN x_formatos_coleccion xfc ON xfc.id = prod.x_producto_formato
         INNER JOIN x_acabados_coleccion xac ON xac.id = prod.x_producto_acabado
         WHERE (POSITION(xcc.x_name IN prod.name) = 0
         OR POSITION(xfc.x_name IN prod.name) = 0
-        OR POSITION(xac.x_name IN prod.name) = 0
-        OR POSITION(prod.x_product_name_desc IN prod.name) = 0)
-        AND prod.active = true;""")
-        products = self._cr.fetchall()
-        if not products:
-            return
-        products = map(lambda s: s[0], products)
-        index = 0
-        for item in self.env['product.template'].browse(products):
-            if index > 2000:
-                break
-            index += 1
-            if item.x_product_rect == 1:
-                item.write({
-                    'name': (item.public_category_id and item.public_category_id.collection_number or '') + ' ' + (
-                                item.public_category_id and item.public_category_id.name or '') + ' ' + (
-                                        item.x_producto_color and item.x_producto_color.x_name or '') + ' ' + (
-                                        item.x_producto_formato and item.x_producto_formato.x_name or '') + ' ' + 'R' + ' ' + (
-                                        item.x_producto_acabado and item.x_producto_acabado.x_name or '') + ' ' + (
-                                        item.x_product_name_desc and item.x_product_name_desc or '')
-                })
-            else:
-                item.write({
-                    'name': (item.public_category_id and item.public_category_id.collection_number or '') + ' ' + (
-                                item.public_category_id and item.public_category_id.name or '') + ' ' + (
-                                        item.x_producto_color and item.x_producto_color.x_name or '') + ' ' + (
-                                        item.x_producto_formato and item.x_producto_formato.x_name or '') + ' ' + (
-                                        item.x_producto_acabado and item.x_producto_acabado.x_name or '') + ' ' + (
-                                        item.x_product_name_desc and item.x_product_name_desc or '')
-                })
+        OR POSITION(xac.x_name IN prod.name) = 0)
+        AND prod.active = true) AS prods"""
