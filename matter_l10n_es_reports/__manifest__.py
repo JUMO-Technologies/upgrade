522660131ba488cefc5a39684f887fb3747ceadc
jorgeluislr <jorgeluislr93@gmail.com>
2022-06-27 13:04:44 +0000

    [ADD]matter_l10n_es_reports module

---
+++ matter_l10n_es_reports/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Matter Reports Spain",
+    "version": "13.0.0.0.1",
+    "summary": "Matter Reports Spain",
+    "description": "Matter Reports Spain",
+    "category": "Accounting",
+    "author": "JUMO Technologies",
+    "website": "https://www.jumotech.com",
+    "license": "LGPL-3",
+    "depends": ["l10n_es_reports"],
+    "data": [
+        "data/account_financial_html_report.xml",
+    ],
+    "installable": True,
+    "auto_install": False
+}
